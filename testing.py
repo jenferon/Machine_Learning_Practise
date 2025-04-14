eba56f6c6bbeda7b4834a996e1bfe65e52e687c6
jennyferon <jenny@feron.co.uk>
2025-04-14 15:23:14 +0100

    first commit

---
+++ testing.py
@@ -0,0 +1,382 @@
+import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import struct
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+import functools
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(DEVICE)
+
+#@title Defining a time-dependent score-based model (double click to expand or collapse)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+import torch.nn.functional as F
+
+def diffusion_coeff(t, sigma):
+  """Compute the diffusion coefficient of our SDE.
+
+  Args:
+    t: A vector of time steps.
+    sigma: The sigma in our SDE.
+  
+  Returns:
+    The vector of diffusion coefficients.
+  """
+  return torch.tensor(sigma**t, device=DEVICE)
+
+def match_tensor_shape(source, target):
+    """
+    Pads or crops `source` tensor so that it matches the spatial dimensions of `target`.
+    
+    Parameters:
+    - source: Tensor to be adjusted (B, C, H1, W1)
+    - target: Reference tensor to match shape to (B, C, H2, W2)
+    
+    Returns:
+    - Adjusted source tensor with shape (B, C, H2, W2)
+    """
+    src_h, src_w = source.shape[-2:]
+    tgt_h, tgt_w = target.shape[-2:]
+
+    # Compute difference
+    diff_h = tgt_h - src_h
+    diff_w = tgt_w - src_w
+
+    # Padding if target is larger
+    if diff_h > 0 or diff_w > 0:
+        pad_top = diff_h // 2
+        pad_bottom = diff_h - pad_top
+        pad_left = diff_w // 2
+        pad_right = diff_w - pad_left
+        source = F.pad(source, [pad_left, pad_right, pad_top, pad_bottom])
+    # Cropping if source is larger
+    elif diff_h < 0 or diff_w < 0:
+        crop_top = -diff_h // 2
+        crop_left = -diff_w // 2
+        crop_bottom = crop_top + tgt_h
+        crop_right = crop_left + tgt_w
+        source = source[..., crop_top:crop_bottom, crop_left:crop_right]
+    
+    return source
+
+class GaussianFourierProjection(nn.Module):
+  """Gaussian random features for encoding time steps."""  
+  def __init__(self, embed_dim, scale=30.):
+    super().__init__()
+    # Randomly sample weights during initialization. These weights are fixed 
+    # during optimization and are not trainable.
+    self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
+  def forward(self, x):
+    x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
+    return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
+
+class Dense(nn.Module):
+  """A fully connected layer that reshapes outputs to feature maps."""
+  def __init__(self, input_dim, output_dim):
+    super().__init__()
+    self.dense = nn.Linear(input_dim, output_dim)
+  def forward(self, x):
+    return self.dense(x)[..., None, None]
+
+
+class ScoreNet(nn.Module):
+  """A time-dependent score-based model built upon U-Net architecture."""
+
+  def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
+    """Initialize a time-dependent score-based network.
+
+    Args:
+      marginal_prob_std: A function that takes time t and gives the standard
+        deviation of the perturbation kernel p_{0t}(x(t) | x(0)).
+      channels: The number of channels for feature maps of each resolution.
+      embed_dim: The dimensionality of Gaussian random feature embeddings.
+    """
+    super().__init__()
+    # Gaussian random feature embedding layer for time
+    self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
+         nn.Linear(embed_dim, embed_dim))
+    # Encoding layers where the resolution decreases
+    self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
+    self.dense1 = Dense(embed_dim, channels[0])
+    self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+    self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
+    self.dense2 = Dense(embed_dim, channels[1])
+    self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+    self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+    self.dense3 = Dense(embed_dim, channels[2])
+    self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+    self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+    self.dense4 = Dense(embed_dim, channels[3])
+    self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])    
+
+    # Decoding layers where the resolution increases
+    self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False)
+    self.dense5 = Dense(embed_dim, channels[2])
+    self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+    self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False)    
+    self.dense6 = Dense(embed_dim, channels[1])
+    self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+    self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False)    
+    self.dense7 = Dense(embed_dim, channels[0])
+    self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+    self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
+    
+    # The swish activation function
+    self.act = lambda x: x * torch.sigmoid(x)
+    self.marginal_prob_std = marginal_prob_std
+  
+  def forward(self, x, t): 
+    # Obtain the Gaussian random feature embedding for t   
+    embed = self.act(self.embed(t))    
+    # Encoding path
+    h1 = self.conv1(x)    
+    ## Incorporate information from t
+    h1 += self.dense1(embed)
+    ## Group normalization
+    h1 = self.gnorm1(h1)
+    h1 = self.act(h1)
+    h2 = self.conv2(h1)
+    #print(h2.shape)
+    h2 += self.dense2(embed)
+    #print(h2.shape)
+    h2 = self.gnorm2(h2)
+    #print(h2.shape)
+    h2 = self.act(h2)
+    h3 = self.conv3(h2)
+    h3 += self.dense3(embed)
+    h3 = self.gnorm3(h3)
+    h3 = self.act(h3)
+    h4 = self.conv4(h3)
+    h4 += self.dense4(embed)
+    h4 = self.gnorm4(h4)
+    h4 = self.act(h4)
+
+    # Decoding path
+    h = self.tconv4(h4)
+    ## Skip connection from the encoding path
+    h += self.dense5(embed)
+    h = self.tgnorm4(h)
+    h = self.act(h)
+    h = match_tensor_shape(h, h3)
+    h = self.tconv3(torch.cat([h, h3], dim=1))
+    h += self.dense6(embed)
+    h = self.tgnorm3(h)
+    h = self.act(h)
+    h = match_tensor_shape(h, h2)
+    h = self.tconv2(torch.cat([h, h2], dim=1))
+    h += self.dense7(embed)
+    h = self.tgnorm2(h)
+    h = self.act(h)
+    h = match_tensor_shape(h, h1)
+    h = self.tconv1(torch.cat([h, h1], dim=1))
+
+    # Normalize output
+    h = h / self.marginal_prob_std(t)[:, None, None, None]
+    return h
+
+class CustomImageDataset(Dataset):
+    def __init__(self, base, data_name, transform=None, target_transform=None):
+       self.transform = transform
+       self.target_transform = target_transform
+       self.image_loc = os.path.join(base, data_name)
+       # Open the file in binary read mode ('rb')
+       self.fid = open(self.image_loc, 'rb')
+       self.dtype_size = 4  # Assuming float32 data type
+
+    def __len__(self):
+       # Get file size and calculate number of images
+       file_size = os.path.getsize(self.image_loc)
+       num_images = file_size // (600 * 600 * self.dtype_size)
+       return num_images # Corrected length calculation
+
+    def __getitem__(self, idx):
+        # Calculate the starting position for the slice
+        offset = idx * 600 * 600 * self.dtype_size
+
+        # Seek to the correct position in the file
+        self.fid.seek(offset)
+
+        # Read only the required slice
+        data = self.fid.read(600 * 600 * self.dtype_size)
+
+        # Unpack the data for the slice into a NumPy array
+        image = struct.unpack('f' * (600 * 600), data)
+        image = np.array(image).reshape(600, 600)
+        #image = np.expand_dims(image, axis=0)
+        image = image[:256,:256]
+        print(image.shape)
+        # Convert to PyTorch tensor and add channel dimension
+        image = torch.from_numpy(image).float().unsqueeze(0)
+
+        label = 'z=12.0'
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+    def __del__(self):
+        # Close the file when the dataset object is deleted
+        self.fid.close()
+def marginal_prob_std(t, sigma):
+  """Compute the mean and standard deviation of $p_{0t}(x(t) | x(0))$.
+
+  Args:
+    t: A vector of time steps.
+    sigma: The sigma in our SDE.
+
+  Returns:
+    The standard deviation.
+  """
+  t = torch.tensor(t, device=DEVICE)
+  return torch.sqrt((sigma**(2 * t) - 1.) / 2. / np.log(sigma))
+
+def loss_fn(model, x, marginal_prob_std, eps=1e-5):
+  """The loss function for training score-based generative models.
+
+  Args:
+    model: A PyTorch model instance that represents a
+      time-dependent score-based model.
+    x: A mini-batch of training data.
+    marginal_prob_std: A function that gives the standard deviation of
+      the perturbation kernel.
+    eps: A tolerance value for numerical stability.
+  """
+  random_t = torch.rand(x.shape[0], device=x.device) * (1. - eps) + eps
+  z = torch.randn_like(x)
+  std = marginal_prob_std(random_t)
+  perturbed_x = x + z * std[:, None, None, None]
+  score = model(perturbed_x, random_t)
+  loss = torch.mean(torch.sum((score * std[:, None, None, None] + z)**2, dim=(1,2,3)))
+  return loss
+
+
+sigma =  25.0#@param {'type':'number'}
+marginal_prob_std_fn = functools.partial(marginal_prob_std, sigma=sigma)
+diffusion_coeff_fn = functools.partial(diffusion_coeff, sigma=sigma)
+
+batch_size = 5
+
+dataset = CustomImageDataset('/home/ppxjf3/diffusion_GAN/','deltaTb_z12.000_N600_L200.0.dat')
+data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+score_model = torch.nn.DataParallel(ScoreNet(marginal_prob_std=marginal_prob_std_fn))
+score_model = score_model.to(DEVICE)
+
+optimizer = Adam(score_model.parameters(), lr=1e-4)
+
+tqdm_epoch = tqdm.trange(2)
+for epoch in tqdm_epoch:
+  avg_loss = 0.
+  num_items = 0
+  for x, y in data_loader:
+    x = x.to(DEVICE)
+    loss = loss_fn(score_model, x, marginal_prob_std_fn)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    avg_loss += loss.item() * x.shape[0]
+    num_items += x.shape[0]
+  # Print the averaged training loss so far.
+  tqdm_epoch.set_description('Average Loss: {:5f}'.format(avg_loss / num_items))
+
+
+""" IMAGE GENERATION  
+
+Need to first define:
+(1) the Pytorch model
+(2) the standard deviation of the pertubation kernel
+"""
+
+#Define the ODE sampler (double click to expand or collapse)
+
+from scipy import integrate
+
+## The error tolerance for the black-box ODE solver
+error_tolerance = 1e-5 #@param {'type': 'number'}
+def ode_sampler(score_model,
+                marginal_prob_std,
+                diffusion_coeff,
+                batch_size=64, 
+                atol=error_tolerance, 
+                rtol=error_tolerance, 
+                device='cuda', 
+                z=None,
+                eps=1e-3):
+  """Generate samples from score-based models with black-box ODE solvers.
+
+  Args:
+    score_model: A PyTorch model that represents the time-dependent score-based model.
+    marginal_prob_std: A function that returns the standard deviation 
+      of the perturbation kernel.
+    diffusion_coeff: A function that returns the diffusion coefficient of the SDE.
+    batch_size: The number of samplers to generate by calling this function once.
+    atol: Tolerance of absolute errors.
+    rtol: Tolerance of relative errors.
+    device: 'cuda' for running on GPUs, and 'cpu' for running on CPUs.
+    z: The latent code that governs the final sample. If None, we start from p_1;
+      otherwise, we start from the given z.
+    eps: The smallest time step for numerical stability.
+  """
+  t = torch.ones(batch_size, device=DEVICE)
+  # Create the latent code
+  if z is None:
+    init_x = torch.randn(batch_size, 1, 28, 28, device=DEVICE) \
+      * marginal_prob_std(t)[:, None, None, None]
+  else:
+    init_x = z
+    
+  shape = init_x.shape
+
+  def score_eval_wrapper(sample, time_steps):
+    """A wrapper of the score-based model for use by the ODE solver."""
+    sample = torch.tensor(sample, device=DEVICE, dtype=torch.float32).reshape(shape)
+    time_steps = torch.tensor(time_steps, device=DEVICE, dtype=torch.float32).reshape((sample.shape[0], ))    
+    with torch.no_grad():    
+      score = score_model(sample, time_steps)
+    return score.cpu().numpy().reshape((-1,)).astype(np.float64)
+  
+  def ode_func(t, x):        
+    """The ODE function for use by the ODE solver."""
+    time_steps = np.ones((shape[0],)) * t    
+    g = diffusion_coeff(torch.tensor(t)).cpu().numpy()
+    return  -0.5 * (g**2) * score_eval_wrapper(x, time_steps)
+  
+  # Run the black-box ODE solver.
+  res = integrate.solve_ivp(ode_func, (1., eps), init_x.reshape(-1).cpu().numpy(), rtol=rtol, atol=atol, method='RK45')  
+  print(f"Number of function evaluations: {res.nfev}")
+  x = torch.tensor(res.y[:, -1], device=DEVICE).reshape(shape)
+
+  return x
+
+sample_batch_size = 5
+sampler = ode_sampler 
+
+## Generate samples using the specified sampler.
+samples = sampler(score_model, 
+                  marginal_prob_std_fn,
+                  diffusion_coeff_fn, 
+                  sample_batch_size, 
+                  device=DEVICE)
+
+## Sample visualization.
+print(samples.shape)
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.imshow(samples[0,0,:,:])
+plt.savefig('testing_image_generation.png', dpi=330)
+plt.show()
